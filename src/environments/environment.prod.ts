be122eebec28e64c0d14a0673304e3363d4ee430
Turbo Xav <xavier.tagliarino@gmail.com>
2020-02-21 11:07:35 +0100

    Environment files defined

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
 export const environment = {
-  production: true
+  production: false,
+  apis: {
+    thingerio: {
+      url: 'https://api.thinger.io',
+    },
+    forecast: {
+      url: 'https://api.meteo-concept.com/api',     
+      token: '25e8258d583eb394ea0135543b4b285374abbd0cb1d1ca96eda34ffcf8a5c747'
+    }
+  },
+  devices: {
+    meteo: 'meteostation'
+  },
+  meteo: {
+    insee: '94081',
+    city: 'Vitry / Seine'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,23 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apis: {
+    thingerio: {
+      url: 'https://api.thinger.io',
+    },
+    forecast: {
+      url: 'https://api.meteo-concept.com/api',     
+      token: '25e8258d583eb394ea0135543b4b285374abbd0cb1d1ca96eda34ffcf8a5c747'
+    }
+  },
+  devices: {
+    meteo: 'meteostation'
+  },
+  meteo: {
+    insee: '94081',
+    city: 'Vitry / Seine'
+  }
 };
 
 /*
