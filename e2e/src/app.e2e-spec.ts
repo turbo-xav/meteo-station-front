f28d1a28d91d183abe9777fef61e4cab8d41840d
Turbo Xav <xavier.tagliarino@gmail.com>
2021-02-27 20:11:43 +0100

    Fixing project name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('new-meteo-station-domotique app is running!');
+    expect(page.getTitleText()).toEqual('meteo-station-domotique app is running!');
   });
 
   afterEach(async () => {
