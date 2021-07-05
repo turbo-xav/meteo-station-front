497a9b3a5726a6d2c1ae82b49091148765a05367
Turbo Xav <xavier.tagliarino@gmail.com>
2021-07-05 11:09:29 +0200

    Fixing tests

--- jest.config.js
+++ jest.config.js
@@ -6,22 +6,27 @@ module.exports = {
     preset: "jest-preset-angular",
     verbose: true,
     setupFilesAfterEnv: ["<rootDir>/setupJest.ts"],
-    
+
+    modulePaths: [
+        "<rootDir>",
+    ],
+
     testPathIgnorePatterns: [
         "<rootDir>/src/test.ts",
         "<rootDir>/e2e/*",
         "<rootDir>/target/*",
-        /*"<rootDir>/src/app/app.component.*",
+        /*"<rootDir>/src/app/app.component.*",*/
         "<rootDir>/src/app/auth/*",
         "<rootDir>/src/app/devices/*",
         "<rootDir>/src/app/home/*",
+        "<rootDir>/src/app/meteo/stats/*",
         "<rootDir>/src/app/meteo/forecast/*",
         "<rootDir>/src/app/meteo/station/*",
         "<rootDir>/src/app/meteo/stats/graph/*",
-        "<rootDir>/src/app/generic/*"*/
+        "<rootDir>/src/app/generic/*"
 
-    ],   
-    
+    ],
+
     coverageDirectory: 'docs/reports/jest/coverage',
     collectCoverage: true,
     collectCoverageFrom: [
@@ -42,8 +47,7 @@ module.exports = {
         'ts-jest': {
             astTransformers: {
                 before: [
-                    'jest-preset-angular/build/InlineFilesTransformer',
-                    'jest-preset-angular/build/StripStylesTransformer',
+
                 ]
             },
         }
