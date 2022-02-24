a8c12a6d5a83450e7c9c79316f12d56ba361c39f
Turbo Xav <xavier.tagliarino@gmail.com>
2022-02-24 11:09:44 +0100

    https port 443

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
   api: {
-    url: 'https://meteo-back.projets-web.fr:2000/api'
-    //url: 'https://localhost:2000/api'
+    url: 'https://meteo-back.projets-web.fr/api'
   }
 };
