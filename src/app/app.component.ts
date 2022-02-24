a18eb085c6f4d2db1532d5087e6940fa6a09f434
Turbo Xav <xavier.tagliarino@gmail.com>
2022-02-24 12:07:12 +0100

    instant translations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,8 @@
-import { Component, ElementRef, ViewChild, Renderer2 } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { environment } from 'src/environments/environment';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  @ViewChild('main', { static: false }) main?: ElementRef;
-
-  constructor(
-    private readonly translateService: TranslateService,
-    private readonly renderer: Renderer2
-  ) {
-    this.translateService.use('fr');
-    console.warn(environment.api);
-  }
-}
+export class AppComponent {}
