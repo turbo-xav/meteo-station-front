0a8f8a7108c621d503e4af937644db2a5cfc1223
Turbo Xav <xavier.tagliarino@gmail.com>
2022-03-14 09:14:00 +0100

    Addin default Admin Users Module

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminComponent } from './admin.component';
 
-const routes: Routes = [{ path: '', component: AdminComponent }];
+const routes: Routes = [
+  { path: '', redirectTo: 'users' },
+  {
+    path: 'users',
+    loadChildren: () =>
+      import('./users/users.module').then((m) => m.UsersModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AdminRoutingModule { }
+export class AdminRoutingModule {}
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AdminRoutingModule } from './admin-routing.module';
-import { AdminComponent } from './admin.component';
 
 @NgModule({
-  declarations: [AdminComponent],
+  declarations: [],
   imports: [CommonModule, AdminRoutingModule]
 })
 export class AdminModule {}
